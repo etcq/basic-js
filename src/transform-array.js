344d41945da6835512f55da67f2cbeea5ea1c3a5
Anton Sushnikov <resonans93@gmail.com>
2025-01-03 17:25:29 +0500

    feat: sum digits, rcursive, encode-line done

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,10 +13,56 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    const error = new Error('\'arr\' parameter must be an instance of the Array!');
+    throw error;
+  }
+  if (arr.length === 0 || arr.every((i) => typeof i !== 'string')) {
+    return arr
+  }
+  let result = [...arr];
+  for (let i = 0; i < result.length; i++) {
+    if (typeof result[i] === 'string' && result[i].includes('--')) {
+      result = move[result[i]](result, i);
+    }
+    
+  }
+  return result;
 }
+
+const move = {
+  '--discard-next': function (arr, target) {
+    if (target === arr.length - 1) {
+      return arr.slice(0, -1); 
+    }
+    return [...arr.slice(0, target), ...arr.slice(target + 2)]
+  },
+
+  '--discard-prev': function (arr, target) {
+    if (target === 0) {
+      return arr.slice(1); 
+    }
+    return [...arr.slice(0, target - 1), ...arr.slice(target + 1)]
+  },
+
+  '--double-prev': function double(arr, target) {
+    if (target === 0) {
+      return arr.slice(1); 
+    }
+    return [...arr.slice(0, target - 1), arr[target - 1], ...arr.slice(target + 1)] 
+   },
+
+  '--double-next': function double(arr, target) {
+    if (target === arr.length - 1) {
+      return arr.slice(0, -1); 
+    }
+    return [...arr.slice(0, target), arr[target + 1], ...arr.slice(target + 1)] 
+   }
+}
+
+
+
 
 module.exports = {
   transform
